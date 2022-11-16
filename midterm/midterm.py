a4eaf21b3a4d7e83b5f3311b0843fb099675d5da
TzuChuan <tzuchuan0619@gmail.com>
2022-11-17 00:03:18 +0800

    v4

--- midterm/midterm.py
+++ midterm/midterm.py
@@ -3,97 +3,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from scipy.signal import argrelextrema
+
+
 if __name__ == '__main__':
-    # cv2.namedWindow('windows', cv2.WINDOW_NORMAL)
 
-    img = cv2.imread('HW_Image/20070907_164013_Cell_79.png', cv2.IMREAD_GRAYSCALE)
-    gray_img = np.copy(img)
+    img_title = ['20070907_160746_Cell_40.png', '20070907_160814_Cell_40.png', '20070907_161304_Cell_40.png',
+                 '20070907_162752_Cell_40.png', '20070907_163631_Cell_28.png', '20070907_164013_Cell_79.png',
+                 '20070907_170041_Cell_114.png', '20070907_171232_Wafer_100.png', '20070907_171317_Wafer_100.png']
 
-    # 使用 sobel 算子
-    # 第一個參數是要作用的影像
-    # 第二個參數是影像深度 使用 16 可避免 overflow 問題
-    # 第三 & 四個參數是控制是否使用兩種面罩的參數
-    # 第五個參數是 mask size
-    kernel_size = 3
-    # x = cv2.Sobel(gray_img, cv2.CV_16S, 1, 0, ksize=kernel_size)
-    #y = cv2.Sobel(gray_img, cv2.CV_16S, 0, 1, ksize=kernel_size)
+    img = cv2.imread(f'HW_Image/{img_title[8]}', cv2.IMREAD_GRAYSCALE)
+    blur = cv2.medianBlur(img, 9)
+    copy_img = np.copy(blur)
+    y_sobel = cv2.Sobel(img, cv2.CV_16S, 0, 1)
+    y_sobel = cv2.convertScaleAbs(y_sobel)
+    dst = cv2.equalizeHist(y_sobel)
 
-    # 轉換為影像原本儲存的格式 uint8
-    # absX = cv2.convertScaleAbs(x)
-    #dst = cv2.convertScaleAbs(y)
-    dst = cv2.medianBlur(img, 9)
-    dst = cv2.equalizeHist(dst)
+    img_ref = dst[:, 300]
 
-    # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
-    # dst = cv2.morphologyEx(dst, cv2.MORPH_CLOSE, kernel, iterations=10)
-    # image_copy = closing.copy()
-    # diff = np.diff(vt_sp)
-    # print(diff)
-    # print(np.where(diff > 50))
-    vt_ref = dst[:, 500]
-    vt_head = dst[:, 500]
-    print(np.where(vt_ref < 90)[0][0])
-    start_idx = np.where(vt_ref < 90)[0][0]
-    print(np.where(vt_ref[start_idx+4000:] < 30))
-    end_idx = np.where(vt_ref[start_idx+4000:] < 30)[0][0]
-    myl_top = np.zeros(1000, dtype=int)
-    diff_top = np.zeros(1000, dtype=int)
-    tmp = 0
-    myl_botton = np.zeros(6000, dtype=int)
-    mean_bot = np.zeros(10001, dtype=int)
-    for i in range(10000, start_idx, -1):
-        ls2 = vt_head[i:i-5:-1]
-        # myl_botton[i] = np.min(ls2)
-        mean_bot[i] = np.sum(ls2)
-        if mean_bot[i] < 250:
-            if abs(tmp-i) > 20:
-                print(mean_bot[i], i)
-            tmp = i
+    start_idx = np.where(img_ref > 220)[0][0]
+    end_idx = np.where(img_ref > 220)[0][-1]
+    # print(end_idx)
+    if np.mean(img_ref[:500]) > 100:
+        start_idx = 520
 
-        # print(ls2)
-    # print(mean_bot[4870:4890])
-    # print(np.where(diff_bot<90))
-    # print(myl_botton[2900:3000])
-    # print(np.where(myl_botton < 20)[0][0])
+    if end_idx > 5000:  # 4160 上方物體長度
+        end_idx -= 4180
 
-
-    # print(vt_ref)
-    # # print(np.where(vt_tail < 20))
-    # start_idx = np.where(vt_head > 200)[0][0]
     # print(start_idx)
-    # vt_tail = dst[start_idx:, 500]
-    # end = np.where(vt_tail < 10)[0][0]
-    # print(end)
-    # # end = np.where(vt_tail < 35)[0][-1]
+    # print(end_idx)
+    roi_img = copy_img[start_idx: end_idx]
 
-    roi_img = dst[start_idx-50:end_idx+start_idx+100]
     vt_1 = roi_img[:, 50]
     vt_2 = roi_img[:, 200]
     vt_3 = roi_img[:, 500]
     vt_4 = roi_img[:, 800]
     vt_5 = roi_img[:, 950]
-    # cv2.imshow("windows", closing)
-    # cv2.waitKey(0)
-    cv2.imwrite('output.png', roi_img)
-    # cv2.destroyAllWindows()
 
-    peaks1, _ = find_peaks(vt_1, height=20, width=5, distance=5)
-    peaks2, _ = find_peaks(vt_2, height=20, width=5, distance=5)
-    peaks3, _ = find_peaks(vt_3, height=20, width=5, distance=5)
-    peaks4, _ = find_peaks(vt_4, height=20, width=5, distance=5)
-    peaks5, _ = find_peaks(vt_5, height=20, width=5, distance=5)
+    cv2.imwrite('ROI_img.png', img[start_idx: end_idx])
+
+    # 找出峰值
+    peaks1, _ = find_peaks(vt_1, height=15, width=5, distance=5)
+    peaks2, _ = find_peaks(vt_2, height=15, width=5, distance=5)
+    peaks3, _ = find_peaks(vt_3, height=15, width=5, distance=5)
+    peaks4, _ = find_peaks(vt_4, height=15, width=5, distance=5)
+    peaks5, _ = find_peaks(vt_5, height=15, width=5, distance=5)
     peak_list = [len(peaks1), len(peaks2), len(peaks3), len(peaks4), len(peaks5)]
 
-    print(peak_list)
-    print(round(np.mean(peak_list)))
-    # print(np.argmax(np.bincount(peak_list)))
+    # print(peak_list)
+    print('總共有', round(np.mean(peak_list[0])), '片')
+    # print('總共有', np.argmax(np.bincount(peak_list)), '片')
+
     plt.imshow(dst, cmap='gray')
     plt.show()
-    plt.plot(peaks3, vt_3[peaks3], "x")
+    # plt.plot(peaks1, vt_1[peaks1], "x", color="red")
+    plt.vlines(peaks3-20, 0, 1020, color="red")
     plt.plot(np.zeros_like(vt_3), "--", color="gray")
-    pimg = plt.imread('output.png')
+    pimg = plt.imread('ROI_img.png')
 
     plt.imshow(np.rot90(pimg), cmap='gray')
     plt.plot(vt_3)
-
     plt.show()
--- midterm/test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from scipy.signal import argrelextrema
-x = np.linspace (0, 50, 100)
-y = np.sin(x)
-#print(y)
-print(argrelextrema(y, np.less))
